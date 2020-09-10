eabf7435fdb82824ea46ca8c495262496472d880
Sebastien Beaudet <1532686@cstj.qc.ca>
2020-09-10 17:05:43 -0400

    Ajoutez des fichiers projet.

---
+++ Projet - Exercice 1/PairImpair.cpp
@@ -0,0 +1,47 @@
+//But : Écrire un programme qui lit un entier et qui détermine et affiche s'il s'agit d'un entier pair ou d'un entier impair.
+//Auteur : Sebastien Beaudet
+//Date : 2020-09-08
+
+#include <iostream> //Inclusion de la bibliothèque pour utiliser les fonctions d'entrées/sorties.
+
+using namespace std; //Pour ne pas avoir à répéter std:: devant 'cout' et 'cin'.
+
+int main(/*Aucun paramètres*/)  //Début du main auquel je dois retourner une valeur.
+{								//Début de mon bloc de code.
+	setlocale(LC_ALL, ""); //Pour la gestion des charactères accentués. 
+	
+	int iNombre; //Déclare ma variable pour stocker le nombre que l'utilisateur va entrer.
+
+	//Demande un entier à l'utilisateur.
+	cout << "Veuillez entrer un entier : ";
+	//Stock ce que l'utilisateur a entré, dans la variable iNombre.
+	cin >> iNombre;
+
+	// (Avec la logique où si un nombre divisé par 2 n'as aucun reste, c'est que celui-ci est un nombre pair. Sinon, il est définitivement impair.)
+	if (iNombre % 2 == 0) // Si le nombre entré par l'utilisateur divisé par 2 a un reste égal à 0 : ( == pour comparer, car = écraserait la variable iNombre)
+	{ //Début de mon bloc de code, lorsque la condition est vraie :
+		cout << iNombre << " est un entier pair !"; //Affiche à l'utilisateur que le nombre est pair.
+	}
+	else // Sinon
+	{ //Début de mon code, si jamais la condition n'est pas vraie :
+		cout << iNombre << " est un entier impair !"; //Affiche à l'utilisateur que le nombre est impair.
+	}
+	 
+	return 0; //Retourne 0 à mon main lorsque le programme se rend jusqu'à cette étape.
+}
+/*  ---------------------Plan de tests---------------------
+ 1. Valeur entrée : 5		Résultat : 5 est un entier impair !
+ 2. Valeur entrée : 4		Résultat : 4 est un entier pair !
+ 3. Valeur entrée : 0		Résultat : 0 est un entier pair !
+ 4. Valeur entrée : -5		Résultat : -5 est un entier impair !
+ 5. Valeur entrée : -6		Résultat : -6 est un entier pair !
+ 6. Valeur entrée : /$		Résultat : 0 est un entier pair !
+ 7. Valeur entrée : hgdc	Résultat : 0 est un entier pair !
+ 8. Valeur entrée : ""		Résultat : 
+ 9. Valeur entrée : " "		Résultat : 
+10. Valeur entrée : "34 4"	Résultat : 34 est un entier pair !
+11. Valeur entrée : 3.1416	Résultat : 3 est un entier impair !
+Conclusion : Si l'entrée n'est pas un 'int' valide, 
+celle-ci est considérée comme étant un 0. Si l'entrée est vide, 
+le programme attend une entrée. Si l'entrée est un réel, seul
+l'entier du réel est considéré.*/
---
+++ Projet - Exercice 2/Vertical.cpp
@@ -0,0 +1,80 @@
+//But : Écrire un programme qui lit un nombre à 5 chiffres. Vérifie qu'il y a 5 chiffres. Affiche le nombre verticalement.
+//Auteur : Sebastien Beaudet
+//Date : 2020-09-08
+
+#include <iostream> //Inclusion de la bibliothèque pour utiliser les fonctions d'entrées/sorties.
+
+using namespace std; //Pour ne pas avoir à répéter std:: devant 'cout' et 'cin'.
+
+int main(/*Aucun paramètres*/)  //Début du main auquel je dois retourner une valeur.
+{								//Début de mon bloc de code.
+	setlocale(LC_ALL, ""); //Pour la gestion des charactères accentués. 
+
+	float fNombre;  //Déclare ma variable pour stocker le nombre que l'utilisateur va entrer.
+					//On met la variable dans un float, pour empêcher les boucles infinies lors de la validation.
+	int iNombre;	//Variable pour stocker l'entier après la validation et utiliser %.
+
+	cout << "Veuillez entrer un entier à 5 chiffres : "; //Demande un entier à l'utilisateur.
+	cin >> fNombre; //Stock ce que l'utilisateur a entré, dans la variable fNombre.
+
+	//Valide si le nombre entré à 5 chiffres. Il doit être compris entre 99999 et 10000. (99999 + 1 = 6 chiffres, 10000 -1 = 4 chiffres. )
+	while ((fNombre > 99999) || (fNombre < 10000))  // Tant que le nombre est plus grand que 99999 ou plus petit que 10000 ou que le nombre divisé par 10000 donne moins que 1.
+	{
+		cout << "Ce nombre ne contient pas 5 chiffres.\nVeuillez entrer un entier à 5 chiffres : "; // Demande un entier à l'utilisateur
+		cin >> fNombre;	//Stock ce que l'utilisateur a entré, dans la variable iNombre.
+	} //Passé cette boucle, nous sommes certain que le nombre stocké a 5 chiffres, si la personne a entrée un nombre.
+
+	iNombre = fNombre; //Visual Studio ne me laissait pas utiliser '%' avec un 'float'. Je transfers donc son contenu dans un 'int'.
+
+	//Affiche le nombre à 5 chiffres sur 5 lignes.
+	//Affiche la première ligne et saute à la suivante.
+	//Logique: Le premier chiffre(x) du nombre 12345 divisé par 10000 donne  (x),2345 (Où 2345 représente le reste du nombre, mais passé la virgule.) Comme c'est une variable entière, seul (x) est affiché.
+	cout << iNombre/10000  << "\n"; 
+
+	//Affiche la deuxième ligne et saute à la suivante.
+	//Logique: Pour le deuxième chiffre(x) du nombre 12345, on divise par 10000 -> 1,(x)345 et on garde le reste, donc (x)345. On divise par 1000 -> (x),345. Comme c'est une variable entière, seul (x) est affiché.
+	cout << (iNombre % 10000)/1000 << "\n"; 
+
+	//Affiche la troisième ligne et saute à la suivante.
+	//Logique: Pour le troisième chiffre(x) du nombre 12345, on divise par 1000 -> 12,(x)45 et on garde le reste, donc (x)45. On divise par 100 -> (x),45. Comme c'est une variable entière, seul (x) est affiché.
+	cout << (iNombre % 1000)/100 << "\n"; 
+
+	//Affiche la quatrième ligne et saute à la suivante.
+	//Logique: Pour le quatrième chiffre(x) du nombre 12345, on divise par 100 -> 123,(x)5 et on garde le reste, donc (x)5. On divise par 10 -> (x),5. Comme c'est une variable entière, seul (x) est affiché.
+	cout << (iNombre % 100)/10 << "\n"; 
+
+	//Affiche la cinquième ligne.
+	//Logique: Pour le deuxième chiffre(x) du nombre 12345, on divise par 10 -> 1234,(x) et on garde le reste, donc (x).
+	cout << iNombre % 10; 
+
+	return 0; //Retourne 0 à mon main lorsque le programme se rend jusqu'à cette étape.
+}
+/*  ---------------------Plan de tests---------------------
+1. Valeur entrée : 5		Résultat : Ce nombre ne contient pas 5 chiffres.
+									   Veuillez entrer un entier à 5 chiffres : (Validation réussie)
+2. Valeur entrée : 445346	Résultat : Ce nombre ne contient pas 5 chiffres.
+									   Veuillez entrer un entier à 5 chiffres : (Validation réussie)
+3. Valeur entrée : 0		Résultat : Ce nombre ne contient pas 5 chiffres.
+									   Veuillez entrer un entier à 5 chiffres : (Validation réussie)
+4. Valeur entrée : -5		Résultat : Ce nombre ne contient pas 5 chiffres.
+									   Veuillez entrer un entier à 5 chiffres : (Validation réussie)
+5. Valeur entrée : -66745	Résultat : Ce nombre ne contient pas 5 chiffres.
+									   Veuillez entrer un entier à 5 chiffres : (Validation réussie)
+6. Valeur entrée : /$		Résultat : Ce nombre ne contient pas 5 chiffres.
+									   Veuillez entrer un entier à 5 chiffres : Ce nombre ne contient pas 5 chiffres. (boucle infinie)
+7. Valeur entrée : hgdc		Résultat : Ce nombre ne contient pas 5 chiffres.
+									   Veuillez entrer un entier à 5 chiffres : Ce nombre ne contient pas 5 chiffres. (boucle infinie)
+8. Valeur entrée : 3.1416	Résultat : Ce nombre ne contient pas 5 chiffres.
+									   Veuillez entrer un entier à 5 chiffres : (Validation réussie)
+9. Valeur entrée : 99999	Résultat : 9
+									   9
+									   9
+									   9
+									   9
+12. Valeur entrée : 12345	Résultat : 1
+									   2
+									   3									   
+									   4									   
+									   5
+Conclusion : Tant que la valeur entrée reste dans le numérique, le programme fonctionne. 
+Dès qu'il s'agit d'un charactère ou d'une chaîne de charactères, le programme 'plante'.*/
