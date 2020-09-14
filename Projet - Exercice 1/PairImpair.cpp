9a86f7989724cf8507155c5c5b2fb20ddfb788d4
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-14 19:58:16 -0400

    Excellent programme ! Très bien commenté et plan de tests complet. Quelques fautes d'orthographe

--- Projet - Exercice 1/PairImpair.cpp
+++ Projet - Exercice 1/PairImpair.cpp
@@ -1,47 +1,47 @@
-//But : Écrire un programme qui lit un entier et qui détermine et affiche s'il s'agit d'un entier pair ou d'un entier impair.
+//But : Ã‰crire un programme qui lit un entier et qui dÃ©termine et affiche s'il s'agit d'un entier pair ou d'un entier impair.
 //Auteur : Sebastien Beaudet
 //Date : 2020-09-08
 
-#include <iostream> //Inclusion de la bibliothèque pour utiliser les fonctions d'entrées/sorties.
+#include <iostream> //Inclusion de la bibliothÃ¨que pour utiliser les fonctions d'entrÃ©es/sorties.
 
-using namespace std; //Pour ne pas avoir à répéter std:: devant 'cout' et 'cin'.
+using namespace std; //Pour ne pas avoir Ã  rÃ©pÃ©ter std:: devant 'cout' et 'cin'.
 
-int main(/*Aucun paramètres*/)  //Début du main auquel je dois retourner une valeur.
-{								//Début de mon bloc de code.
-	setlocale(LC_ALL, ""); //Pour la gestion des charactères accentués. 
+int main(/*Aucun paramÃ¨tres*/)  //DÃ©but du main auquel je dois retourner une valeur.
+{								//DÃ©but de mon bloc de code.
+	setlocale(LC_ALL, ""); //FR : Pour la gestion des charactÃ¨res accentuÃ©s. 
 	
-	int iNombre; //Déclare ma variable pour stocker le nombre que l'utilisateur va entrer.
+	int iNombre; //DÃ©clare ma variable pour stocker le nombre que l'utilisateur va entrer.
 
-	//Demande un entier à l'utilisateur.
+	//Demande un entier Ã  l'utilisateur.
 	cout << "Veuillez entrer un entier : ";
-	//Stock ce que l'utilisateur a entré, dans la variable iNombre.
+	//FR : Stock ce que l'utilisateur a entrÃ©, dans la variable iNombre.
 	cin >> iNombre;
 
-	// (Avec la logique où si un nombre divisé par 2 n'as aucun reste, c'est que celui-ci est un nombre pair. Sinon, il est définitivement impair.)
-	if (iNombre % 2 == 0) // Si le nombre entré par l'utilisateur divisé par 2 a un reste égal à 0 : ( == pour comparer, car = écraserait la variable iNombre)
-	{ //Début de mon bloc de code, lorsque la condition est vraie :
-		cout << iNombre << " est un entier pair !"; //Affiche à l'utilisateur que le nombre est pair.
+	// FR : (Avec la logique oÃ¹ si un nombre divisÃ© par 2 n'as aucun reste, c'est que celui-ci est un nombre pair. Sinon, il est dÃ©finitivement impair.)
+	if (iNombre % 2 == 0) // Si le nombre entrÃ© par l'utilisateur divisÃ© par 2 a un reste Ã©gal Ã  0 : ( == pour comparer, car = Ã©craserait la variable iNombre)
+	{ //DÃ©but de mon bloc de code, lorsque la condition est vraie :
+		cout << iNombre << " est un entier pair !"; //Affiche Ã  l'utilisateur que le nombre est pair.
 	}
 	else // Sinon
-	{ //Début de mon code, si jamais la condition n'est pas vraie :
-		cout << iNombre << " est un entier impair !"; //Affiche à l'utilisateur que le nombre est impair.
+	{ //DÃ©but de mon code, si jamais la condition n'est pas vraie :
+		cout << iNombre << " est un entier impair !"; //Affiche Ã  l'utilisateur que le nombre est impair.
 	}
 	 
-	return 0; //Retourne 0 à mon main lorsque le programme se rend jusqu'à cette étape.
+	return 0; //Retourne 0 Ã  mon main lorsque le programme se rend jusqu'Ã  cette Ã©tape.
 }
 /*  ---------------------Plan de tests---------------------
- 1. Valeur entrée : 5		Résultat : 5 est un entier impair !
- 2. Valeur entrée : 4		Résultat : 4 est un entier pair !
- 3. Valeur entrée : 0		Résultat : 0 est un entier pair !
- 4. Valeur entrée : -5		Résultat : -5 est un entier impair !
- 5. Valeur entrée : -6		Résultat : -6 est un entier pair !
- 6. Valeur entrée : /$		Résultat : 0 est un entier pair !
- 7. Valeur entrée : hgdc	Résultat : 0 est un entier pair !
- 8. Valeur entrée : ""		Résultat : 
- 9. Valeur entrée : " "		Résultat : 
-10. Valeur entrée : "34 4"	Résultat : 34 est un entier pair !
-11. Valeur entrée : 3.1416	Résultat : 3 est un entier impair !
-Conclusion : Si l'entrée n'est pas un 'int' valide, 
-celle-ci est considérée comme étant un 0. Si l'entrée est vide, 
-le programme attend une entrée. Si l'entrée est un réel, seul
-l'entier du réel est considéré.*/
+ 1. Valeur entrÃ©e : 5		RÃ©sultat : 5 est un entier impair !
+ 2. Valeur entrÃ©e : 4		RÃ©sultat : 4 est un entier pair !
+ 3. Valeur entrÃ©e : 0		RÃ©sultat : 0 est un entier pair !
+ 4. Valeur entrÃ©e : -5		RÃ©sultat : -5 est un entier impair !
+ 5. Valeur entrÃ©e : -6		RÃ©sultat : -6 est un entier pair !
+ 6. Valeur entrÃ©e : /$		RÃ©sultat : 0 est un entier pair !
+ 7. Valeur entrÃ©e : hgdc	RÃ©sultat : 0 est un entier pair !
+ 8. Valeur entrÃ©e : ""		RÃ©sultat : 
+ 9. Valeur entrÃ©e : " "		RÃ©sultat : 
+10. Valeur entrÃ©e : "34 4"	RÃ©sultat : 34 est un entier pair !
+11. Valeur entrÃ©e : 3.1416	RÃ©sultat : 3 est un entier impair !
+Conclusion : Si l'entrÃ©e n'est pas un 'int' valide, 
+celle-ci est considÃ©rÃ©e comme Ã©tant un 0. Si l'entrÃ©e est vide, 
+le programme attend une entrÃ©e. Si l'entrÃ©e est un rÃ©el, seul
+l'entier du rÃ©el est considÃ©rÃ©.*/
